6f2f0c6eadc8ef05133406041ccf6eacbfcf513f
Daniel Campoverde <alx@sillybytes.net>
2019-05-04 17:55:24 -0500

    Stop words removal

--- src/Text/Mining/StopWords.hs
+++ src/Text/Mining/StopWords.hs
@@ -1,11 +1,17 @@
 module Text.Mining.StopWords where
 
-import Data.Set     (Set, fromList)
-import Data.Text    (Text, toLower, words)
+import Data.Set     (Set, fromList, member)
+import Data.Text    (Text, toLower, unwords, words)
 import Data.Text.IO (readFile)
-import Prelude      hiding (readFile, words)
+import Prelude      hiding (readFile, unwords, words)
 
 type StopWordsLexicon = Set Text
+
+removeStopWords :: StopWordsLexicon -> Text -> Text
+removeStopWords lexicon
+    = unwords
+    . filter (\w -> not $ toLower w `member` lexicon)
+    . words
 
 loadLexiconFile :: FilePath -> IO StopWordsLexicon
 loadLexiconFile fp
