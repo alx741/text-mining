560d2b95271cf83d3609448b0c7fd2912896993b
Daniel Campoverde <alx@sillybytes.net>
2019-05-04 18:06:24 -0500

    Stop words ignoring diacritics

--- src/Text/Mining/StopWords.hs
+++ src/Text/Mining/StopWords.hs
@@ -6,11 +6,18 @@ import Data.Text.IO (readFile)
 import Prelude      hiding (readFile, unwords, words)
 
 type StopWordsLexicon = Set Text
+type StopWordsLexiconNoDiacritics = Set Text
 
 removeStopWords :: StopWordsLexicon -> Text -> Text
 removeStopWords lexicon
     = unwords
     . filter (\w -> not $ toLower w `member` lexicon)
+    . words
+
+removeStopWordsIgnoreDiacritics :: StopWordsLexiconNoDiacritics -> Text -> Text
+removeStopWordsIgnoreDiacritics lexicon
+    = unwords
+    . filter (\w -> not $ (removeDiacritics . toLower) w `member` lexicon)
     . words
 
 loadLexiconFile :: FilePath -> IO StopWordsLexicon
@@ -19,3 +26,13 @@ loadLexiconFile fp
     . fmap toLower
     . words
     <$> readFile fp
+
+loadLexiconFileIgnoreDiacritics :: FilePath -> IO StopWordsLexiconNoDiacritics
+loadLexiconFileIgnoreDiacritics fp
+    = fromList
+    . fmap (removeDiacritics . toLower)
+    . words
+    <$> readFile fp
+
+removeDiacritics :: Text -> Text
+removeDiacritics = undefined
