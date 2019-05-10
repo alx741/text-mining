65a3824783d79d5d6519a995fb9b383cce6e1ee8
Daniel Campoverde <alx@sillybytes.net>
2019-05-10 10:24:41 -0500

    Fix StopWords tests

--- test/Text/Mining/StopWordsSpec.hs
+++ test/Text/Mining/StopWordsSpec.hs
@@ -9,19 +9,21 @@ spec :: Spec
 spec = do
     describe "removeStopWords" $ do
         it "Removes stop words that case match" $ do
-            removeStopWords trivialEnglishLexicon  "I am here" `shouldBe` "here"
-            removeStopWords trivialSpanishLexicon  "Eso está ahí" `shouldBe` "Eso ahí"
+            removeStopWords trivialEnglishLexicon "I am here"    `shouldBe` "here"
+            removeStopWords trivialSpanishLexicon "Eso está ahí" `shouldBe` "Eso ahí"
         it "Removes stop words that doesn't case match" $ do
-            removeStopWords trivialEnglishLexicon  "i Am here" `shouldBe` "here"
-            removeStopWords trivialSpanishLexicon  "Eso ESTÁ ahí" `shouldBe` "Eso ahí"
+            removeStopWords trivialEnglishLexicon "i Am here"    `shouldBe` "here"
+            removeStopWords trivialSpanishLexicon "Eso ESTÁ ahí" `shouldBe` "Eso ahí"
         it "Matches diacritics" $ do
-            removeStopWords trivialSpanishLexicon  "Eso esta ahí" `shouldBe` "Eso esta ahí"
+            removeStopWords trivialSpanishLexicon "Eso esta ahí" `shouldBe` "Eso esta ahí"
 
     describe "removeStopWordsIgnoreDiacritics" $ do
         it "Removes stop words regardless of diacritics" $ do
-            removeStopWords trivialSpanishLexicon  "Eso está ahí" `shouldBe` "Eso ahí"
+            removeStopWordsIgnoreDiacritics
+                spanishLexiconNoDiacritics "Eso está ahí" `shouldBe` "Eso ahí"
         it "Removes stop words regardless of diacritics and casing" $ do
-            removeStopWords trivialSpanishLexicon  "Eso ESTÁ ahí" `shouldBe` "Eso ahí"
+            removeStopWordsIgnoreDiacritics
+                spanishLexiconNoDiacritics "Eso ESTÁ ahí" `shouldBe` "Eso ahí"
 
 
 trivialEnglishLexicon :: StopWordsLexicon
@@ -38,6 +40,6 @@ trivialSpanishLexicon = lexiconFromList
 
 spanishLexiconNoDiacritics :: StopWordsLexiconNoDiacritics
 spanishLexiconNoDiacritics = lexiconFromListIgnoreDiacritics
-    [ "un" , "soy" , "y" , "está" , "como" , "en"
+    [ "un" , "soy" , "y" , "esta" , "como" , "en"
     , "pero" , "por" , "es" , "Yo", "poder", "hacer"
     ]
